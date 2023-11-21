a6675987af0f763e09a71d8fa2963b985145618d
Junsung Park / Daanta <daanta@naver.com>
2023-11-22 00:15:24 +0900

    231122_0015 Succeed to hot swap with not using Spring Loaded

--- spring_legacy_sample/src/main/java/com/sample/web/HomeController.java
+++ spring_legacy_sample/src/main/java/com/sample/web/HomeController.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import java.util.Locale;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +22,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		log.info("s sdㄴㄴㄴs오모모모 {}.", locale);
+		log.info("s asf노노도도도도도도돋 {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
